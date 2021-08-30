687493677c72f3492cfaecce4db46db60f8c4f11
Shyam Krishnan <49896229+shyamtarams@users.noreply.github.com>
2021-08-30 17:34:59 +0530

    Update settings.py

--- testingproject/settings.py
+++ testingproject/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'shyam.krishnan141@gmail.com'  
-EMAIL_HOST_PASSWORD = 'agoanbird'  
+EMAIL_HOST_USER = '****'  
+EMAIL_HOST_PASSWORD = '****'  
 
 
 
